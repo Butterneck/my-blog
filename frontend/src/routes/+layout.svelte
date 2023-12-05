2b502b9a9802a7f59a1edca4bd90f279246ca225
Filippo Pinton <filippo.pinton@varda.ag>
2023-12-06 00:56:55 +0100

    Add tailwind

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import "../app.pcss";
   import "../assets/styles/global.css";
   import Header from "$lib/components/Header.svelte";
   import Container from "$lib/components/Container.svelte";
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,21 +1,21 @@
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: vitePreprocess({}),
 
 	kit: {
 		adapter: adapter({
-		  fallback: "index.html",
+			fallback: 'index.html'
 		}),
 		alias: {
-		  $lib: "src/lib",
-		  $data: "src/data",
-		},
-	  },
+			$lib: 'src/lib',
+			$data: 'src/data'
+		}
+	}
 };
 
 export default config;
