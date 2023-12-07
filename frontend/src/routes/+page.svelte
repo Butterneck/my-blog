e3c8d1c79e721d786487139704c9b34e93c65cd7
Filippo Pinton <filippo.pinton@varda.ag>
2023-12-07 03:19:32 +0100

    Started working on post archive

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,6 +6,8 @@
 	import PostList from '$lib/components/PostList.svelte';
 	import BigContainer from '$lib/components/BigContainer.svelte';
 	import RankedPost from '$lib/components/RankedPost.svelte';
+	import WidePost from '$lib/components/WidePost.svelte';
+	import SmallPost from '$lib/components/SmallPost.svelte';
 
 	const meta = {
 		title: `Home | ${blogMetaData.blogTitle}`,
@@ -46,10 +48,12 @@
 	}}
 />
 
+<!-- LATEST POSTS -->
 <BigContainer>
 	<PostList posts={data.posts} />
 </BigContainer>
 
+<!-- TRENDING POSTS -->
 <div
 	class="flex md:w-auto main-container max-width-main pb-10 pt-10 sub-top-border sm:mx-12 mx-6 xl:mx-auto"
 >
@@ -83,3 +87,76 @@
 		{/each}
 	</div>
 </div>
+
+
+<!-- ALL POSTS -->
+<!-- <div class="w-full flex main-container max-width-main pb-4 pt-10 sub-top-border posts-grid lg:w-auto lg:mx-12 md:block sm:mx-6"> -->
+
+<div class="flex md:w-auto main-container max-width-main pb-10 pt-10 sub-top-border sm:mx-12 mx-6 xl:mx-auto posts-grid">	
+	<div class="w-full mb-4 grid-left">
+		{#each data.posts.slice(1, 10) as post}
+
+			<SmallPost {post} />
+		{/each}
+	  <div class="w-full text-center">
+		<!-- {/* <WideCard {...({} as Post)} /> */} -->
+		<button
+		  class="my-4 mx-auto p-2 cursor-pointer w-48 max-w-full load-more main-black font-semibold rounded flex flex-row justify-between items-center"
+		>
+		  <div class="flex-grow text-center">Show More</div>
+		  <img class="ml-3" src="/down_arrow.svg" />
+		</button>
+	  </div>
+	</div>
+	<div class="grid-right hidden md:block">
+	  <div class="sticky top-8 p-8 about-bg flex flex-col">
+		<div class="w-full flex mb-4 flex-row items-center">
+		  <img class="mr-3" src="bookmarks.svg" />
+		  <div>
+			<p class="heading-text text-sm leading-4 uppercase tracking-wide sm:text-xs">
+			  About Reddium
+			</p>
+		  </div>
+		</div>
+		<div class="w-full pb-6">
+		  <p class="text-sm">
+			Ever wanted to browse Reddit while studying at Starbucks? Or
+			while sitting on the subway to work? Worried that people around
+			you would judge the subreddits you browse and the posts you
+			read?
+			<br />
+			<br />
+			{`Reddium is a Medium-themed Reddit client. The Reddium interface
+			converts Reddit posts, discussions, and memes into well-crafted
+			articles. Medium's layout feels a little more readable than
+			Reddit's, removing all distractions and clutter. It also
+			bypasses Reddit's frustrating mobile browser.`}
+			<br />
+			<br />I hope you enjoy this project! Feel free to suggest any
+			features or report bugs on GitHub.
+		  </p>
+		</div>
+		<div class="w-full pb-6 hidden">
+		  <img class="w-4/12 float-right" src="/signature.png" />
+		</div>
+		<a
+		  href="https://github.com/eightants/reddium/"
+		  target="_blank"
+		  rel="noopener noreferrer"
+		>
+		  <button class="mt-2 mx-1 p-2 pl-0 pb-3 cursor-pointer w-full max-w-full btn-black text-white rounded">
+			✨ Star on GitHub
+		  </button>
+		</a>
+		<a
+		  href="https://ko-fi.com/eightants"
+		  target="_blank"
+		  rel="noopener noreferrer"
+		>
+		  <button class="mt-2 mx-1 p-2 pl-0 pb-3 cursor-pointer w-full max-w-full btn-outline-black text-white rounded">
+			☕ Buy me a coffee
+		  </button>
+		</a>
+	  </div>
+	</div>
+  </div>
